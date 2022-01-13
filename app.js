4d8db054c10a1fefd8ea4c4d908ffad893113715
Misha Krasnonos <muzishutka7@gmail.com>
2022-01-13 15:00:26 +0200

    up

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ require('dotenv').config()
 
 const contactsRouter = require('./routes/api/contacts')
 
-// const e = process.env
-// const PORT = e.PORT || 8083
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
@@ -25,10 +23,4 @@ app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err
   res.status(status).json({ message })
 })
-// app.listen(PORT, (err) => {
-//   if (err) {
-//     console.error('Error at a server launch', err)
-//   }
-// console.log(`Server work at port ${PORT}!`)
-// })
 module.exports = app
